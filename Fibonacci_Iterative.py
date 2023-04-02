669f9cca3b240b2eab0e3d819d93e3d264fa653a
Shain Sahagun <95956735+m3mentomor1@users.noreply.github.com>
2023-04-02 13:13:25 +0800

    Add files via upload

---
+++ Fibonacci_Iterative.py
@@ -0,0 +1,50 @@
+
+# ------------------------------------------------------
+# File Name: Fibonacci_1.py
+# ------------------------------------------------------
+# Date Finished: 07-29-2022
+# ------------------------------------------------------
+# Description:
+# This is a simple iteratively calculated fibonacci 
+# sequence generator that takes an integer input from 
+# a user, then uses that input as the number of terms 
+# in the sequence.
+# ------------------------------------------------------
+
+print("\n------- Fibonacci Sequence Generator -------\n")
+
+while True:
+    try:   
+        # Number of terms                                                                                                     
+        nth = int(input("Enter no. of terms to generate\n(Note: Term count starts at 0): "))   
+        if nth <= 0:                                                  
+            print("Input must be a positive integer!\n")              
+        else:                                                         
+            break                                                     
+    except:                                                           
+        print("Invalid input! Try again.\n")                          
+
+
+def main():
+    
+    sequence = [0, 1] # First 2 terms as a list.
+    count = 2         # Iteration/loop counter.
+
+    msg = "\nFibonacci Sequence:"
+
+    if nth == 1:
+        print(msg + "\n", [sequence[0]], sep="") # Prints the first term of the sequence if number of terms is 1
+    
+    else:
+        print(msg)
+
+        # Iterates the calculation of the next terms in the sequence:
+        while count < nth:
+            sequence.append(sequence[count-1] + sequence[count-2])  # Adds the newly calculated terms to the list. 
+
+        print(sequence)
+
+main()
+
+print("\n--------------------------------------------\n")
+
---
+++ Fibonacci_Recursive.py
@@ -0,0 +1,59 @@
+
+# ------------------------------------------------------
+# File Name: Fibonacci_2.py
+# ------------------------------------------------------
+# Date Finished: 07-30-2022
+# ------------------------------------------------------
+# Description:
+# This is a simple recursively calculated fibonacci 
+# sequence generator that takes an integer input from 
+# a user, then uses that input as the number of terms 
+# in the sequence.
+# ------------------------------------------------------
+
+from functools import lru_cache
+
+print("\n---------- Fibonacci Sequence Generator ----------\n")
+
+while True:
+    try:       
+        # Number of terms                                                                              
+        n = int(input("Enter no. of terms to generate\n(Note: Term count starts at 0): "))      
+        if n < 1:                                                   
+            print("Input must be a positive integer!\n")            
+        else:                                                       
+            break                                                   
+    except:                                                         
+        print("Invalid input! Try again.\n")                        
+
+
+@lru_cache(maxsize=None)    # Optimizes the recursion implementation
+
+# Recursive function:
+def f(n): 
+
+    # Base case 1:
+    if n == 1:
+        return 0 # Returns the 1st term of the sequence if number of terms is 1
+
+    # Base case 2:
+    elif n == 2:
+        return 1 # Returns the 2nd term of the sequence if number of terms is 2
+    
+    # Recursive case:
+    elif n > 2:
+        # Calculates & returns the next terms 
+        # of the sequence using a recursive call:
+        return f(n - 1) + f(n - 2) 
+
+
+print("\nFibonacci Sequence:")
+
+# Prints the sequence starting from the first 
+# term up to the last calculated term based 
+# on the inputted number of terms:
+for n in range(1, n+1):     
+    print(f(n), end="  ")   
+
+print("\n\n--------------------------------------------------\n")
+
